0d3b31fd7ab5ff9124b7ad3b3c5c6c0a2a0bde0b
Marc0-03 <marcusnylund44@gmail.com>
2021-09-24 12:12:43 +0200

    FINNALY IT WORKS

--- 11ty-start/.eleventy.js
+++ 11ty-start/.eleventy.js
@@ -1,4 +1,9 @@
 module.exports = function(eleventyConfig) {
+
+    eleventyConfig.addWatchTarget("./src/sass/");
+
+    eleventyConfig.addPassthroughCopy("./src/sass/");
+    
     return {
         dir: {
             input: "src",
